e24e94d70e413178cd5e416ec50665e800a1847c
rob passmore <rpassmore@gmail.com>
2018-06-17 18:29:43 +0100

    fix init paths

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 redis-server &
 # Generate a new encryption key every time the container starts
-gohole -gkey
+/app/gohole -gkey
 # Run GoHole
-gohole -s -c /root/gohole_config.json
+/app/gohole -s -c /root/gohole_config.json
